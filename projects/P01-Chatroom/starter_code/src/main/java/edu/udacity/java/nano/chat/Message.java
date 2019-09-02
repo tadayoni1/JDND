1f8c52223602837e9ccc78fccaf825569e619e3b
Pedram <tadayoni@gmail.com>
2019-09-01 23:25:56 -0700

    feat: added Message model

--- projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
+++ projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -4,5 +4,21 @@ package edu.udacity.java.nano.chat;
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
+
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
